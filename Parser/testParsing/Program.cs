69a71168b780f305aac6fbd9c18bb0558997558a
Chalpak <kirikchalapov152@gmail.com>
2021-12-01 20:01:19 +0300

    Parser
    parsing

---
+++ Parser/testParsing/Program.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace testParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb w = new HtmlWeb();
+            var hd = w.Load("https://pokemongolife.ru/pokemony/");
+
+            var cites = hd.DocumentNode.SelectNodes("//a[@class='navbar-brand']");
+
+            foreach (var cite in cites)
+                Console.WriteLine(cite.InnerText);
+        }
+    }
+}
